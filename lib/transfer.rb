3cfbf52cc63cf8af8f32fe95b7c422abb1e96bca
Sehrish Balouch <sehrishbalouch4@gmail.com>
2020-01-01 20:14:37 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -2,5 +2,6 @@ class Transfer
   attr_accessor :sender, :reciever, :amount, :status 
   def initialize(sender, reciever, amount)
     @seender = sender
-    @reciver = 
+    @reciver = reciever
+    
 end
