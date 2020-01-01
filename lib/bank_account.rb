a8a823962a1bbbbe08e01915d13fdfe3dc180c4c
Sehrish Balouch <sehrishbalouch4@gmail.com>
2020-01-01 20:18:44 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -6,5 +6,19 @@ class BankAccount
     @name = name
     @account_hash = account_hash
   end 
-  
+  def deposit(money)
+    @balance += money
+  end
+
+  def display_balance
+    "Your balance is $#{@balance}."
+  end
+
+  def valid?
+    @status == "open" && @balance > 0 ? true : false
+  end
+
+  def close_account
+    @status = "closed"
+  end
 end
