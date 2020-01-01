58891189bf8ea9de46fd59e2948ba8cbee80d601
Sehrish Balouch <sehrishbalouch4@gmail.com>
2020-01-01 20:10:20 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,6 +1,10 @@
 class BankAccount
-  attr_accessor :balance, :status, :name 
-  def initialize 
-    
+  attr_accessor :balance, :status, :name, :account_hash
+  def initialize(balance, status, name, account_hash)
+    @balance = balance
+    @status = status
+    @name = name
+    @account_hash = account_hash
+  end 
   
 end
